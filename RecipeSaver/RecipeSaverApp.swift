d8298cf1d37103d1421e77110a8da904efac6bf2
halaxhenry <halaxhenry@gmail.com>
2022-03-07 19:49:23 +0900

    Initial Commit

---
+++ RecipeSaver/RecipeSaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeSaverApp.swift
+//  RecipeSaver
+//
+//  Created by Seungchul Ha on 3/7/22.
+//
+
+import SwiftUI
+
+@main
+struct RecipeSaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
